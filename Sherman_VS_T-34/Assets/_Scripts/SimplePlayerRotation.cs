b51b6e9e170c2382d1cac2a85cc151dcac870368
nuclearMonkeys <lyndond@uci.edu>
2019-04-13 09:24:36 -0700

    Inital Push

---
+++ Sherman_VS_T-34/Assets/_Scripts/SimplePlayerRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerRotation : MonoBehaviour
+{
+    private Vector3 lookAt;
+    // Start is called before the first frame update
+    void Start()
+    {  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate() {
+        lookAt = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angleRad = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x);
+
+        float angleDeg = (180 / Mathf.PI) * angleRad;
+
+        this.transform.rotation = Quaternion.Euler(0,0,angleDeg);
+    }
+}
