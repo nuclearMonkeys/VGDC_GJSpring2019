a4990ba522a39e5ca656e70efa3159c961a60cf4
nuclearMonkeys <lyndond@uci.edu>
2019-04-13 15:02:30 -0700

    MonoSingleton V2, UIManager Basics Down & Random Level Progression Works

--- Sherman_VS_T-34/Assets/_Scripts/UIManager.cs
+++ Sherman_VS_T-34/Assets/_Scripts/UIManager.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoSingleton<UIManager>
 {
+    public GameObject[] panels;
+    public int curPanelsIndex = 0;
 
+    public void SwitchPanels(int indexOfPanelToActivate)
+    {
+        panels[curPanelsIndex].SetActive(false);
+        panels[indexOfPanelToActivate].SetActive(true);
+        curPanelsIndex = indexOfPanelToActivate;
+    }
 }
