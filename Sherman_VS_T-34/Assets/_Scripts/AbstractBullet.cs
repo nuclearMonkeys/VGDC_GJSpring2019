1c7befb2e7e3cdcfb2b4be48e7f8baa66c2a2821
nuclearMonkeys <lyndond@uci.edu>
2019-04-13 21:51:59 -0700

    Implemented Health System

--- Sherman_VS_T-34/Assets/_Scripts/AbstractBullet.cs
+++ Sherman_VS_T-34/Assets/_Scripts/AbstractBullet.cs
@@ -4,6 +4,7 @@ public class AbstractBullet : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
+    public int damage = 10;
     GameManager gameManager;
 
     // Start is called before the first frame update
@@ -15,7 +16,7 @@ public class AbstractBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //rb.velocity = new Vector2() * speed;
+        rb.velocity = Vector2.up * speed;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -23,20 +24,24 @@ public class AbstractBullet : MonoBehaviour
         if (other.gameObject.CompareTag("PlayerOne")) {
             other.gameObject.GetComponent<SimplePlayerMovement>().gameManager.numOfTWins++;
             gameManager = other.gameObject.GetComponent<SimplePlayerMovement>().gameManager;
-            Destroy(other.gameObject);
-            Destroy(this);
-            gameManager.StartGame();
+            other.gameObject.GetComponent<PlayerHealth>().currentHealth -= 10;
+            //Destroy(other.gameObject);
+            //Destroy(this);
+            //gameManager.StartGame();
         }
         if (other.gameObject.CompareTag("PlayerTwo")) {
             other.gameObject.GetComponent<PlayerMovement2>().gameManager.numOfShermanWins++;
             gameManager = other.gameObject.GetComponent<PlayerMovement2>().gameManager;
-            Destroy(other.gameObject);
-            Destroy(this);
-            gameManager.StartGame();
+            other.gameObject.GetComponent<PlayerHealth>().currentHealth -= 10;
+            //Destroy(other.gameObject);
+            //Destroy(this);
+            //gameManager.StartGame();
         }
         if (other.gameObject.CompareTag("Wall")) {
             print("hit");
-            speed = -speed;
+            rb.velocity = Vector2.Reflect(rb.velocity, other.contacts[0].normal);
+            
+            //speed = -speed;
         }
         
     }
--- Sherman_VS_T-34/Assets/_Scripts/PlayerHealth.cs
+++ Sherman_VS_T-34/Assets/_Scripts/PlayerHealth.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int maxHealth = 120;
+    public int currentHealth = 100;
+
+    public GameObject playerSlider;
+
+    private void Start()
     {
-        
+        if (this.CompareTag("PlayerOne")) {
+            playerSlider = GameObject.FindGameObjectWithTag("PlayerOneHealth");
+        }
+        if (this.CompareTag("PlayerTwo")) {
+            playerSlider = GameObject.FindGameObjectWithTag("PlayerTwoHealth");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        playerSlider.GetComponent<Slider>().value = currentHealth;
+        if (currentHealth  <= 0 ) {
+            
+            Destroy(this.gameObject);
+        }
     }
+
 }
