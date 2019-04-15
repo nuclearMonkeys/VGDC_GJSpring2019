821adb901dcc30a6b375f60b507f2a6a99b686ec
Dane Carstens <dcarsten@uci.edu>
2019-04-14 17:50:56 -0700

    minor ui addition

--- Sherman_VS_T-34/Assets/_Scripts/Dane Test/HealthManager.cs
+++ Sherman_VS_T-34/Assets/_Scripts/Dane Test/HealthManager.cs
@@ -7,12 +7,21 @@ public class HealthManager : MonoSingleton<HealthManager>
     public Transform bar;
     public float currentHealth = 1f;
     public GameObject enemyVars;
-
-    protected override void OnStart()
+    public string string1 = "Player 1 wins: 0";
+    public string string2 = "Player 2 wins: 0";
+    public int player1_x = 0;
+    public int player2_x = 0;
+    public int size = 35;
+    void OnGUI()
     {
-        
+        // Make a text field that modifies stringToEdit.
+        player1_x = Screen.width / 16;
+        player2_x = Screen.width / 2;
+        string1 = "Player 1 wins: " + GameManager.Instance.numOfShermanWins; //Screen.height
+        string2 = "Player 2 wins: " + GameManager.Instance.numOfTWins;
+        string1 = GUI.TextField(new Rect(player1_x, 10, 200, 20), string1, size);
+        string2 = GUI.TextField(new Rect(player2_x, 10, 200, 20), string2, size);
     }
-
     void Update()
     {
         if (tag == "PlayerOne")
@@ -28,14 +37,26 @@ public class HealthManager : MonoSingleton<HealthManager>
     {
         if (other.CompareTag("Bullet"))
         {
+            
             if (currentHealth > 0)
             {
-                currentHealth -= .1f;
+                
+                float damage = (1 / (10 * currentHealth + 1));
+
+                currentHealth -= damage;
+                
             }
-            if (currentHealth <= 0)
+            if (currentHealth <= 0 && (tag == "PlayerOne"))
             {
                 GameManager.Instance.numOfTWins++;
+                currentHealth = 0f;
                 Debug.Log(GameManager.Instance.numOfTWins++);
+            }
+            if (currentHealth <= 0 && (tag == "PlayerTwo"))
+            {
+                GameManager.Instance.numOfShermanWins++;
+                currentHealth = 0f;
+                Debug.Log(GameManager.Instance.numOfShermanWins++);
             }
         }
     }
